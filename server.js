f2778344dfaed71d53bafc49c64ccd1fa7a16152
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-06-19 20:47:44 -0400

    updated server.js with mongodb database

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/heroku_2tqn0l4x', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
